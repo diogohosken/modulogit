71edad06989f948e48f7a75a2609017d92e17eb0
Diogo Hosken <diogoolivehosken@hotmail.com>
2019-04-23 05:09:08 -0300

    primeiro commit em um projeto que fiz pra testar

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+$(function(){
+
+	$('#botao').bind('click', function(){
+		var alt= $('#altura').val();
+		var pes= $('#peso').val();
+
+		alt=alt.replace(',', '.');
+		pes=pes.replace(',', '.');
+
+		var result= pes/(alt*alt);
+		result=result.toFixed(2);
+
+		if(result < 17){
+			var situ="Muito abaixo do peso";
+		} else if(result >= 17 && result < 18.5){
+			var situ="Abaixo do peso";
+		} else if(result >= 18.5 && result < 25){
+			var situ="Peso normal";
+		} else if(result >= 25 && result < 30){
+			var situ="Acima do peso";
+		} else if(result >= 30 && result < 35){
+			var situ="Obesidade I";
+		} else if(result >= 35 && result < 40){
+			var situ="Obesidade II";
+		} else if(result >= 40){
+			var situ="Obesidade III";
+		}
+
+		$('#resultado').html('Seu IMC é ' +result+ ' kg/m², status: ' +situ);
+	})
+
+});
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE hmtl>
+<html>
+	<head lang="pt-br">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, user-scalable=0"/>
+		<title>IMC</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	</head>
+	<body>
+		<div class="container centro">
+			<div class="obj">
+				<h1 >Calculo de IMC</h1>
+				<div class="corpo_obj">
+					Altura em metros<br>
+					<input type="text" name="altura" id="altura" autofocus placeholder="ex: 1.70 metros"><br><br>
+
+					Peso em quilos<br>
+					<input type="text" name="peso" id="peso" placeholder="ex: 80 Kg"><br><br>
+
+				</div>	
+					<button id="botao" class="btn btn-success">Calcular IMC</button><br><br>
+				<div id="resultado"></div>
+			</div>
+
+		</div>
+
+
+
+
+
+
+
+
+
+
+		<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="assets/js/script.js"></script>
+
+	</body>
+</html>
